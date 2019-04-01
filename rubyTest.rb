304a231c5b16e680c0746b3cdc5f511464ccf80a
SoftSwiss <evgenij.vinogradov@softswiss.com>
2019-04-01 17:32:11 +0300

    second commit

--- rubyTest.rb
+++ rubyTest.rb
@@ -1,1 +1,33 @@
-puts 3+5
+puts 365 * 24
+puts 365 * 10 * 24 * 60
+puts 365 * 27 * 24 * 60 * 60
+puts 943000000 / 60 / 60 / 24 / 365
+puts 'Hello'
+puts '"      "Hello"      "'
+a = 5
+puts a
+a = 6
+puts a
+var = 5
+var1 = '5'
+puts (var + var1.to_i).to_f
+puts '99.99'.to_f
+puts 'hello.'.to_f
+=begin
+puts 'Hello, How your name?'
+name = gets
+puts 'My name ' + name
+puts 'You are welcome ' + name
+=end
+var2 = 'краб'
+puts var2.reverse
+puts var2.length
+puts 'Hello'.upcase
+puts 'HeLLo'.downcase
+puts 'HeLLo'.swapcase
+puts 'привет'.capitalize
+lineWeigth = 30
+puts 'hello'.center(lineWeigth)
+puts 'hello my name Eugene'.center(30)
+puts "hello".rjust(lineWeigth)
+puts 'hello'.ljust(lineWeigth)
